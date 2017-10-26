2145cea418768215d352cf8757ad16ebb6633acb
valery <valery.fremaux@gmail.com>
2017-10-26 10:30:19 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version    = 2017050900;
-$plugin->requires   = 2016112900; // Moodle 3.2 and above.
+$plugin->requires   = 20170505000; // Moodle 3.3 and above.
 $plugin->component  = 'block_multicourse_navigation';
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '3.2.0 (Build 2017050900)';
+$plugin->release    = '3.3.0 (Build 2017050900)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0000';
+$plugin->codeincrement = '3.3.0000';
